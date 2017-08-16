78ca11c15ca05693c4790955b943e75d86d2750b
shichijoaria <15608176098@163.com>
2017-08-16 17:01:01 +0800

    refresh

--- src/stringbuilder/Main.java
+++ src/stringbuilder/Main.java
@@ -6,6 +6,9 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        Map map=new HashMap<String,String>();
+        map.put("url","http://baidu.com");
+        map.put("btn","url");
+        new StringBuilder().OA365(map,"rtn");
     }
 }
--- src/stringbuilder/StringBuilder.java
+++ src/stringbuilder/StringBuilder.java
@@ -10,38 +10,34 @@ import java.util.Set;
  */
 public class StringBuilder {
 
-    public void OA365(Map map){
-        try { // 防止文件建立或读取失败，用catch捕捉错误并打印，也可以throw
-
-            File writeName = new File("src/finish.html"); // 相对路径，如果没有则要建立一个新的output。txt文件
+    public void OA365(Map map,String name){//name 拼接后字符串变量名
+        try {
+            File writeName = new File("src/stringbuilder/finish.html"); // 相对路径，如果没有则要建立一个新的output。txt文件
             writeName.createNewFile(); // 创建新文件
             BufferedWriter out = new BufferedWriter(new FileWriter(writeName));
 
-            /* 读入TXT文件 */
-            String pathname = "src/demo.html"; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
-            File filename = new File(pathname); // 要读取以上路径的input。txt文件
+            //读取要拼接的代码
+            String pathname = "src/stringbuilder/demo.html"; //读取路径
+            File filename = new File(pathname);
             InputStreamReader reader = new InputStreamReader(
                     new FileInputStream(filename)); // 建立一个输入流对象reader
             BufferedReader br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言
             Set set = map.entrySet();
-            for(Iterator iter = set.iterator(); iter.hasNext();) {
-                Map.Entry entry = (Map.Entry) iter.next();
-                out.write("var rtn =''\r");
-            }
+            out.write("var "+name+" ='';\r");
             String line = "";
             line = br.readLine();
             if (line!=null){
-                out.write("rtn+=\""+line+"\"\r");
+                out.write(name+"+=\""+line+"\";\r");
             }
-            for(Iterator iter = set.iterator(); iter.hasNext();) {
-                Map.Entry entry = (Map.Entry) iter.next();
-                while (line != null) {
-                    line = br.readLine(); // 一次读入一行数据
-                    if (line != null) {
-                        line = line.replaceAll("\"", "\\\\" + "\"");
+            while (line != null) {
+                line = br.readLine(); // 一次读入一行数据
+                if (line != null) {
+                    line = line.replaceAll("\"", "\\\\" + "\"");
+                    for(Iterator iter = set.iterator(); iter.hasNext();) {
+                        Map.Entry entry = (Map.Entry) iter.next();
                         line = line.replaceAll((String) entry.getKey(), "\"+" + entry.getValue() + "+\"");
-                        out.write("rtn+=\"" + line + "\"\r"); // \r\n即为换行
                     }
+                    out.write(name+"+=\"" + line + "\";\r"); // \r\n即为换行
                 }
             }
             out.flush(); // 把缓存区内容压入文件
